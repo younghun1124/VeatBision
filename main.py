c96f84cdf549377b852b1f59a94b417c19ab9f0e
Hun-X1 <younghun4218@gmail.com>
2024-06-01 14:19:43 +0900

    메인함수 수정

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from object_detection import colorChange,size_changer
 
 # 비디오 캡처
 if __name__ == '__main__':
-    cap = cv2.VideoCapture('data/testvid2.mp4')
+    cap = cv2.VideoCapture('data/testvid.mp4')
     musicpath='music\Diviners feat. Contacreast - Tropic Love [NCS Release].mp3'
     tempo, beat_times = extract_beat_timing(musicpath)
     mode=0
@@ -31,7 +31,7 @@ if __name__ == '__main__':
         beat_effect_coeff= get_beat_effect_coefficient(frame_count,beat_times, tempo)
         results = model.predict(img)
         
-        key = cv2.waitKey(33) & 0xFF
+        key = cv2.waitKey(1) & 0xFF
         if key == ord(' '):
             cv2.waitKey()
             
@@ -41,7 +41,7 @@ if __name__ == '__main__':
             mode=1
         elif key == ord('2'):
             mode=2
-            img = size_changer(img,results, beat_effect_coeff)
+            
         elif key == ord('q'):
             break
 
@@ -50,7 +50,7 @@ if __name__ == '__main__':
         elif mode == 2:
             img = size_changer(img,results, beat_effect_coeff)
         
-        img = filter_image_with_lut(img)  # Apply LUT filter to the image
+        # img = filter_image_with_lut(img)  # Apply LUT filter to the image
         cv2.imshow('veatbision', img)
     cap.release()
     cv2.destroyAllWindows()
